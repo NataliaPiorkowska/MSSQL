0d10fd1a7155acbefbc2d604dacc35205f1e2c73
NataliaPiorkowska <77283697+NataliaPiorkowska@users.noreply.github.com>
2021-03-30 21:49:53 +0200

    Add files via upload

---
+++ SQLQuery4.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE students2;
+USE students;
+
+CREATE TABLE StudentsInfo(
+StudentID int,
+StudentName varchar(30),
+ParentName varchar (30),
+PhoneNumber bigint,
+AddressofStudent varchar (100),
+City varchar (30),
+Country varchar(30),
+);
+
+
