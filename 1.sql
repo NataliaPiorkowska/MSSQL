9eba1e03b62bb565690b31d439077c0265718b94
NataliaPiorkowska <77283697+NataliaPiorkowska@users.noreply.github.com>
2021-03-30 22:06:25 +0200

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,3 +1,10 @@
+/* DAY 1 - 30.03.2021*/
+/* I've learned:
+   - datatypes,
+   - how to create databases and tables,
+   - how to set primary key (and how it is),
+   - how to set columns and its datatypes. */
+   
 CREATE DATABASE students2;
 USE students;
 
@@ -9,6 +16,7 @@ PhoneNumber bigint,
 AddressofStudent varchar (100),
 City varchar (30),
 Country varchar(30),
+PRIMARY KEY (StudentID)
 );
 
 
